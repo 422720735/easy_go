5fdc4734dc3b3687bc3a3261d34d203a1b9720ca
422720735 <422720735@qq.com>
2019-11-28 13:03:26 +0800

    修改beego的配置项 dev 静态文件位置

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	_ "easy_go/routers"
+
 	"github.com/astaxie/beego"
 )
 
-func main() {
-    beego.BConfig.RunMode = "dev"
-	beego.Run()
+func inint() {
+	beego.BConfig.RunMode = beego.AppConfig.String("runmode")          // 环境
+	beego.BConfig.WebConfig.ViewsPath = beego.AppConfig.String("view") // 静态目录
 }
-
+func main() {
+	beego.Run(":" + beego.AppConfig.String("httpport"))
+}
