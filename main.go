a4152e750c9dead478e9b9371eb1aa624124f642
422720735 <422720735@qq.com>
2019-11-29 13:15:05 +0800

    插件解压

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 	"github.com/astaxie/beego"
 )
 
-func inint() {
-	beego.BConfig.RunMode = beego.AppConfig.String("runmode")          // 环境
-	beego.BConfig.WebConfig.ViewsPath = beego.AppConfig.String("view") // 静态目录
-}
+
 func main() {
+    // beego.BConfig.RunMode = beego.AppConfig.String("runmode")          // 环境
+    beego.BConfig.RunMode = "dev"
+	beego.BConfig.WebConfig.ViewsPath = "template" // 静态目录
 	beego.Run(":" + beego.AppConfig.String("httpport"))
 }
