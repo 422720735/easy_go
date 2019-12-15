6678764b9d2bb2fa26234473d5be6a9b98d202b5
liutiyong <422720735@qq.com>
2019-12-15 23:04:03 +0800

    css

---
+++ controllers/article.go
@@ -0,0 +1,30 @@
+package controllers
+
+import "github.com/astaxie/beego"
+
+type ArticleController struct {
+	beego.Controller
+}
+
+func (c *ArticleController) Get() {
+	c.Layout = "base/articleLayout.html"
+	c.TplName = "articleDetails.html"
+	c.LayoutSections = make(map[string]string)
+
+	c.LayoutSections["Loading"] = "transition/TweenMax.html"
+
+	c.LayoutSections["Header"] = "style/article_ListCss.html"
+	c.LayoutSections["FirstScreen"] = "style/TweenMaxCss.html"
+	c.LayoutSections["ArticleDetails"] = "style/ArticleDetailsCss.html"
+
+	// canvas背景
+	c.LayoutSections["canvasNest"] = "script/canvasNest.html"
+
+	c.LayoutSections["initialize"] = "transition/initialize.html"
+
+	// svg init加载动画
+	c.LayoutSections["TweenMaxJs"] = "script/TweenMaxJs.html"
+
+	// 分页器
+	c.LayoutSections["pagingJs"] = "script/pagingJs.html"
+}
