56ce9e1302d1a8b13d427039517c8faec0c1295b
liutiyong <422720735@qq.com>
2019-12-20 00:08:47 +0800

    文章详情页注释svg动画。

--- controllers/article.go
+++ controllers/article.go
@@ -10,13 +10,14 @@ func (c *ArticleController) Get() {
 	c.Layout = "base/articleLayout.html"
 	c.TplName = "articleDetails.html"
 	c.LayoutSections = make(map[string]string)
-
-	c.LayoutSections["Loading"] = "transition/TweenMax.html"
+    // svg init加载动画
+// 	c.LayoutSections["Loading"] = "transition/TweenMax.html"
 	// h1
 	c.LayoutSections["ArticelH1"] = "articleDetailsH1.html"
 
 	c.LayoutSections["Header"] = "style/article_ListCss.html"
-	c.LayoutSections["FirstScreen"] = "style/TweenMaxCss.html"
+	// svg init加载动画
+// 	c.LayoutSections["FirstScreen"] = "style/TweenMaxCss.html"
 	c.LayoutSections["ArticleDetails"] = "style/ArticleDetailsCss.html"
 
 
@@ -26,7 +27,7 @@ func (c *ArticleController) Get() {
 	c.LayoutSections["initialize"] = "transition/initialize.html"
 
 	// svg init加载动画
-	c.LayoutSections["TweenMaxJs"] = "script/TweenMaxJs.html"
+// 	c.LayoutSections["TweenMaxJs"] = "script/TweenMaxJs.html"
 
 	// 分页器
 	c.LayoutSections["pagingJs"] = "script/pagingJs.html"
