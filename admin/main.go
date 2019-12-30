8989bbe864c747a96ada62cf9097e92be27ed420
liutiyong <422720735@qq.com>
2019-12-30 20:45:50 +0800

    admin error.html页面

--- admin/main.go
+++ admin/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"easy_go/admin/controllers"
 	_ "easy_go/admin/routers"
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	beego.ErrorController(&controllers.ErrorController{})
+	beego.BConfig.RunMode = "dev"
 	beego.BConfig.WebConfig.ViewsPath = "template"
 	beego.Run(":" + beego.AppConfig.String("httpport"))
 }
--- blog/main.go
+++ blog/main.go
@@ -11,6 +11,6 @@ func main() {
 	// beego.BConfig.RunMode = beego.AppConfig.String("runmode")          // 环境
 	beego.ErrorController(&controllers.ErrorController{})
     beego.BConfig.RunMode = "dev"
-	// beego.BConfig.WebConfig.ViewsPath = "template" // 静态目录
+	beego.BConfig.WebConfig.ViewsPath = "template" // 静态目录
 	beego.Run(":" + beego.AppConfig.String("httpport"))
 }
