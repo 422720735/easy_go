7d82bdd40fc442cc2940cdbf8b7bbeb5751e6190
422720735 <422720735@qq.com>
2020-03-12 13:11:20 +0800

    路由权限配置

--- admin/controllers/menu.go
+++ admin/controllers/menu.go
@@ -26,3 +26,22 @@ func (c *MenuController) Get() {
 	c.LayoutSections["Script"] = "script/welcome.html"
 }
 
+func (c *MenuController) Add() {
+	c.Layout = "layout/mainLayout.html"
+
+	c.TplName = "pages/menuSetting/menuAdd.html"
+	c.LayoutSections = make(map[string]string)
+	// menu
+	c.LayoutSections["LeftMenu"] = "layout/leftSideMenuLayout.html"
+	// header
+	c.LayoutSections["HeaderLayout"] = "layout/headerLayout.html"
+	// footer
+	c.LayoutSections["FooterLayout"] = "layout/footerLayout.html"
+	// css
+	c.LayoutSections["BaseStyle"] = "style/baseStyle.html"
+	// js
+	c.LayoutSections["BaseScript"] = "script/baseScript.html"
+
+	c.LayoutSections["Style"] = "style/menuSetting.html"
+	c.LayoutSections["Script"] = "script/menuSettingAdd.html"
+}
