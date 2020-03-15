7a354d5df04396813e240a5ad5ce8cfde52e9acf
liutiyong <422720735@qq.com>
2020-03-16 00:12:40 +0800

    配置文件

--- admin/db/conn.go
+++ admin/db/conn.go
@@ -1,3 +1,3 @@
 package db
 
-
+import _ "github.com/go-sql-driver/mysql"
--- admin/main.go
+++ admin/main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"easy_go/admin/controllers"
+	"easy_go/admin/lib"
 	_ "easy_go/admin/routers"
 	"github.com/astaxie/beego"
+	"runtime"
 )
 
 func main() {
-	beego.ErrorController(&controllers.ErrorController{})
-	beego.BConfig.RunMode = "dev"
-	beego.BConfig.WebConfig.ViewsPath = "template"
-	beego.Run(":" + beego.AppConfig.String("httpport"))
-}
+	goos := runtime.GOOS
+	system := ""
+	if goos == "linux" {
+		system = "admin_prod"
+	} else {
+		system = "admin_dev"
+	}
+	env := lib.Conf.Read(system, "env")
+	port := lib.Conf.Read(system, "httpport")
+	viewsPath := lib.Conf.Read(system, "viewspath")
 
+	beego.ErrorController(&controllers.ErrorController{})
+	beego.BConfig.RunMode = env
+	beego.BConfig.WebConfig.ViewsPath = viewsPath
+	beego.Run(":" + port)
+}
