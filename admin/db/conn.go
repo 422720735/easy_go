df2216870de07de855631859d02f8af0d7487c05
422720735 <422720735@qq.com>
2020-03-16 13:09:05 +0800

    数据库 gorm链接。

--- admin/db/conn.go
+++ admin/db/conn.go
@@ -1,3 +1,17 @@
 package db
 
-import _ "github.com/go-sql-driver/mysql"
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	Db *gorm.DB
+)
+
+func init() {
+	Db, err := gorm.Open("mysql", "root:123456@/dbname?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+}
