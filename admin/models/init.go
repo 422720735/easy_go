df54d440769736bdd1f621ed026b38982c61b504
422720735 <422720735@qq.com>
2020-03-19 17:56:02 +0800

    数据库

--- admin/models/init.go
+++ admin/models/init.go
@@ -1,20 +1,35 @@
 package models
 
 import (
-	"log"
+	"easy_go/admin/lib"
+	"runtime"
 
-	"github.com/xormplus/xorm"
+	"github.com/jinzhu/gorm"
 )
 
 var (
-	DbConn *xorm.Engine
+	DbConn *gorm.DB
 	err    error
 )
 
 func init() {
-	DbConn, err = xorm.NewEngine("mysql", "root:123@/test?charset=utf8")
+	goos := runtime.GOOS
+	mysql := ""
+	if goos == "linux" {
+		mysql = "mysql_prod"
+	} else {
+		mysql = "mysql_dev"
+	}
+	username := lib.Conf.Read(mysql, "username")
+	password := lib.Conf.Read(mysql, "password")
+	dataname := lib.Conf.Read(mysql, "dataname")
+	port := lib.Conf.Read(mysql, "port")
+	host := lib.Conf.Read(mysql, "host")
+	dns := username + ":" + password + "@tcp(" + host + ":" + port + ")/" + dataname + "?parseTime=true&charset=utf8&loc=Local"
+	DbConn, err := gorm.Open("mysql", dns)
 	if err != nil {
-		log.Fatalf("db error: %#v\n", err.Error())
 		panic(err)
 	}
+	DbConn.DB().SetMaxIdleConns(10)
+	DbConn.DB().SetMaxOpenConns(10)
 }
