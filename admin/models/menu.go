408cc840bdc3186ce8ef028a8fb7a4b32bac6d0e
422720735 <422720735@qq.com>
2020-03-19 18:03:53 +0800

    数据库操作

--- admin/models/menu.go
+++ admin/models/menu.go
@@ -3,12 +3,12 @@ package models
 import "time"
 
 type MenuSetting struct {
-	Id int 	`json:"id"`
-	MenuName string `json:"menu_name"`
-	Path string `json:"path"`
-	Icon string `json:"icon"`
-	ChildStatus int `json:"child_status"`
-	Sort int `json:"sort"`
+	Id          int       `json:"id"`
+	MenuName    string    `json:"menu_name"`
+	Path        string    `json:"path"`
+	Icon        string    `json:"icon"`
+	ChildStatus int       `json:"child_status"`
+	Sort        int       `json:"sort"`
 	CreatedTime time.Time `json:"created_time"`
-	UpdateTime time.Time `json:"update_time"`
+	UpdateTime  time.Time `json:"update_time"`
 }
