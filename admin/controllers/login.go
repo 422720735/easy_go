43b5648a42779c87d07a4aefbad9f16c7144add4
liutiyong <422720735@qq.com>
2020-03-25 23:20:03 +0800

    登陆还没完全实现。

--- admin/controllers/login.go
+++ admin/controllers/login.go
@@ -1,16 +1,12 @@
 package controllers
 
 import (
-	"easy_go/admin/common"
-	"easy_go/admin/servers"
-	"easy_go/admin/transform"
-	"easy_go/md5"
 	"github.com/astaxie/beego"
 	// "time"
 )
 
 type LoginController struct {
-	common.BaseController
+	beego.Controller
 }
 
 func (c *LoginController) Get() {
@@ -25,23 +21,28 @@ func (c *LoginController) Get() {
 }
 
 func (c *LoginController) HandleLogin() {
-	_ = c.GetStrings("checkbox")
-	msg, err := common.Unmarshal(&c.Controller)
-	username, _ := transform.InterToString(msg["username"])
-	password, _ := transform.InterToString(msg["password"])
-	if username == "" || len(username) < 6 || password == "" || len(password) < 6 {
-		beego.Info("------", username, password)
-		c.History("账号或密码不合法", "/login")
-		return
-	}
-	// 加密后的密码
-	processPwd, err := servers.SelectUserMd5Pwd(username, md5.Md5(password, common.SECRET_KEY))
-	if err != nil {
-		beego.Info(processPwd, "+++++")
-	}
-	if username == "admin" && password == "123456" {
-		c.Redirect("/workplace", 302)
-	} else {
-		c.Redirect("/login", 302)
-	}
+	username := c.GetString("username")
+	password := c.GetString("password")
+	beego.Info(username, password, "-------------")
+	//_ = c.GetStrings("checkbox")
+	//msg, err := common.Unmarshal(&c.Controller)
+	//username,_:=transform.InterToString(msg["username"])
+	//username,_:=transform.InterToString(msg["password"])
+	//if username == "" || len(username) < 6 || password == "" || len(password) < 6 {
+	//	beego.Info("------")
+	//	c.History("账号或密码不合法", "/login")
+	//	return
+	//}
+	//// 加密后的密码
+	//processPwd, err := servers.SelectUserMd5Pwd(username, md5.Md5(password, common.SECRET_KEY))
+	//if err != nil {
+	//	beego.Info(processPwd,"+++++")
+	//}
+	//if username == "admin" && password == "123456" {
+	//	c.Redirect("/workplace", 302)
+	//} else {
+	//	c.Redirect("/login", 302)
+	//}
+
+	c.Redirect("/login", 302)
 }
