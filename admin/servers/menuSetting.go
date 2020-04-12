98ac8cd9e5c6ab30069d558de219193ea52d8ced
liutiyong <422720735@qq.com>
2020-04-12 22:29:37 +0800

    情怀

--- admin/servers/menuSetting.go
+++ admin/servers/menuSetting.go
@@ -1,6 +1,19 @@
 package servers
 
+import (
+	"easy_go/admin/db"
+	"easy_go/admin/models"
+	"time"
+)
+
 // 新增路由菜单数据
 func InsertMenu(menuName, path, icon string, isChildSwitch, isHotSwitch bool) {
-	
+	var m models.MenuSetting
+	m.MenuName = menuName
+	m.Path = path
+	m.Icon = icon
+	m.ChildStatus = isChildSwitch
+	m.Hot = isHotSwitch
+	m.UpdateTime.Scan(time.Now())
+	db.DbConn.Create(&m)
 }
