eade273ae4ed647c31955d0755fb5411e0b20062
刘体勇 <422720735@qq.com>
2020-06-07 09:44:02 +0800

    文章添加 草稿箱，发布，垃圾桶机制。

--- admin/models/menu.go
+++ admin/models/menu.go
@@ -11,7 +11,7 @@ type MenuSetting struct {
 	Path        string       `json:"path";gorm:"size: 128"`
 	Icon        string       `json:"icon";gorm:"size: 64"`
 	Visible     bool         `json:"visible";gorm:"default:'0'"` // 0 默认下架
-	State      bool         `json:"state";gorm:"default:'0'"`  // 0 默认不删除 1 软删除
+	State       bool         `json:"state";gorm:"default:'0'"`   // 0 默认不删除 1 软删除
 	Sort        int          `json:"sort";gorm:"AUTO_INCREMENT"` // 排序
 	ChildStatus bool         `json:"child_status"`
 	Hot         bool         `json:"hot";gorm:"size 20"`
@@ -19,8 +19,7 @@ type MenuSetting struct {
 	UpdateTime  sql.NullTime `json:"update_time"`
 }
 
-
 type ArticleTypeAndMenuName struct {
 	ArticleType
-	MenuName    string       `json:"menu_name";gorm:"size:128"`
+	MenuName string `json:"menu_name";gorm:"size:128"`
 }
