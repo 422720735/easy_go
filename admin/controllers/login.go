ba901c2987deecefcf083152dade94572eee6464
刘体勇 <422720735@qq.com>
2020-06-23 11:36:49 +0800

    新版本登录

--- admin/controllers/login.go
+++ admin/controllers/login.go
@@ -4,6 +4,7 @@ import (
 	"easy_go/admin/common"
 	"easy_go/admin/logger"
 	"easy_go/admin/servers"
+	"easy_go/admin/transform"
 	"easy_go/md5"
 	"time"
 )
@@ -22,25 +23,46 @@ func (c *LoginController) Get() {
 }
 
 func (c *LoginController) HandleLogin() {
-	username := c.GetString("username")
-	password := c.GetString("password")
-	check := c.GetStrings("checkbox")
-	if username == "" || len(username) < 6 || password == "" || len(password) < 6 {
-		c.History("账号或密码不合法", "")
+	msg, err := common.Unmarshal(&c.Controller)
+	if err != nil {
+		logger.Info("账号或密码不合法", err.Error())
+		c.Error("账号或密码不合法")
 		return
 	}
+
+	username, err := transform.InterToString(msg["username"])
+	if err != nil {
+		logger.Info("账号或密码不合法", err.Error())
+		c.Error("账号或密码不合法")
+		return
+	}
+
+	password, err := transform.InterToString(msg["password"])
+	if err != nil {
+		logger.Info("账号或密码不合法", err.Error())
+		c.Error("账号或密码不合法")
+		return
+	}
+
+	check := c.GetStrings("checkbox")
+	if username == "" || len(username) < 6 || password == "" || len(password) < 6 {
+		c.Error("账号或密码不合法")
+		return
+	}
+	
 	// 加密后的密码
 	process, err := servers.SelectUserMd5Pwd(username, md5.Md5(password, common.SECRET_KEY))
 	if err != nil {
 		logger.Error("用户:"+username+"加密失败", err.Error())
-		c.History("账号或密码不合法", "")
+		c.Error("账号或密码不合法")
 		return
 	}
+
 	// 跟数据库的比对
 	user, err := servers.SelectUserMd5Pwd(username, process.PassWord)
 	if err != nil {
 		logger.Warn("用户:"+username+"比对密码出错", err.Error())
-		c.History("账号或密码不合法", "")
+		c.Error("账号或密码不合法")
 		return
 	}
 
@@ -53,7 +75,7 @@ func (c *LoginController) HandleLogin() {
 			tokenString, err := common.NewCurrentCookie(user)
 			if err != nil {
 				logger.Warn("用户登录创建token失败", err.Error())
-				c.History("未知异常", "")
+				c.Error("未知异常")
 				return
 			}
 			// 把token 登录时间，登录ip，更新sql时间，更新到用户表里，走一次sql更新，sql成功后继续下面的操作。
@@ -64,7 +86,7 @@ func (c *LoginController) HandleLogin() {
 			err = servers.LoginRecord(user)
 			if err != nil {
 				logger.Info("用户记录登陆信息失败", err.Error())
-				c.History("未知异常", "")
+				c.Error("未知异常")
 				return
 			}
 			c.Ctx.SetCookie("auth", tokenString, time.Second*60*60)
@@ -72,8 +94,8 @@ func (c *LoginController) HandleLogin() {
 		// 存session
 		c.SetSession("userId", user.Id)
 		c.SetSession("userName", user.UserName)
-		c.Redirect("/workplace", 302)
+		c.Success("登录成功！")
 	} else {
-		c.Redirect("/login", 302)
+		c.Error("登录失败！")
 	}
 }
--- admin/gulp/src/js/api/login_register.js
+++ admin/gulp/src/js/api/login_register.js
@@ -37,25 +37,30 @@ $('#register-btn').click(function () {
     }
 })
 
-// $('#login-btn.btn.btn-primary.btn-block').click(function () {
-//     alert(12)
-//     const username = $('#username').val()
-//     const password = $('#password').val()
-//     // 是不是记住密码
-//     const checkbox = $('#checkbox-signin').is(':checked')
-//     if (username && username !== '' && password && password !== '') {
-//         const data = JSON.stringify({
-//             username: username,
-//             password: password,
-//             checkbox: checkbox
-//         })
-//         $.ajax({
-//             url: '/login',
-//             data: data,
-//             method: 'POST',
-//             success: function (res) {
-//                 console.log(res)
-//             }
-//         })
-//     }
-// })
+// 登录
+$('#login-btn.btn.btn-primary.btn-block').click(function () {
+    const username = $('#username').val()
+    const password = $('#password').val()
+    // 是不是记住密码
+    const checkbox = $('#checkbox-signin').is(':checked')
+    if (username && username !== '' && password && password !== '') {
+        const data = JSON.stringify({
+            username: username,
+            password: password,
+            checkbox: checkbox
+        })
+        $.ajax({
+            url: '/login',
+            data: data,
+            method: 'POST',
+            success: function (res) {
+                if (res.code === Ok) {
+                    window.message.set(res)
+                    window.location.href = '/workplace'
+                } else {
+                    window.message.error(res)
+                }
+            }
+        })
+    }
+})
