af23d0eb3e07ff395b45b166ce3b46cc77656019
刘体勇 <422720735@qq.com>
2020-07-07 22:57:55 +0800

    梳理清楚了评论回复数据的查询

--- blog/servers/comment.go
+++ blog/servers/comment.go
@@ -54,12 +54,13 @@ func AddReply(content string, userId, commentId, replyId int, replyType models.R
 }
 
 /**
-1.查询到的评论list信息userId存在重复我们需要去重。
-2.根据这个user_id,comment_id在去查询相关回复。
+1.分页查询评论信息，每条评论id是唯一的，而这个id会去关联回复表中评论id，
+2.我们可以根据上面的逻辑拿到每条评论下的所有回复信息。回复信息本来就不多，所以不需要分页，直接查询全部。
+3.拿到了评论表、回复表两个表的数据，我们下一步进行组装数据。
 */
 func SelectCommentList(article_id, size, page int) error {
 	var commentList []*models.Comment
-	//var reply []*models.Reply
+	var reply []*models.Reply
 
 	commentSql := db.DbConn.Model(&models.Comment{}).Where("article_id = ?", article_id)
 	err := commentSql.Error
@@ -68,7 +69,7 @@ func SelectCommentList(article_id, size, page int) error {
 		return err
 	}
 
-	commentSql = commentSql.Limit(size).Offset((page - 1)* size).Order("created_time desc").Find(&commentList)
+	commentSql = commentSql.Limit(size).Offset((page - 1) * size).Order("created_time desc").Find(&commentList)
 	err = commentSql.Error
 
 	if err != nil {
@@ -78,12 +79,28 @@ func SelectCommentList(article_id, size, page int) error {
 
 	// 去重
 	var user_array_old []int
+	var comment_id []int
 	for _, value := range commentList {
+		// SELECT * from replies WHERE user_id = 1 and comment_id in (4,3,2,1)
+		// SELECT * from replies WHERE user_id in (4,2,1,3) and comment_id in (4,3,2,1)
+		//db.DbConn.Model(&models.Reply{}).Where("user_id = ? and comment_id = ?", value.UserId, value.Id)
+
+		//
+		beego.Info(value.Id, value.UserId)
 		user_array_old = append(user_array_old, value.UserId)
+		// 把与之相对应的10条分页评论id装到切片里，我们根据评论的id去查询相对应的回复。
+		comment_id = append(comment_id, value.Id)
 	}
 
+	// 现在我们查询到了与之对应到回复，我们对这个数据进行遍历，我们把这个数据装到对应的评论下。
+	err = db.DbConn.Model(&models.Reply{}).Where("comment_id in (?)", comment_id).Find(&reply).Error
+	if err != nil {
+		logger.Info(err.Error())
+		beego.Info(err.Error())
+	}
 	// 调用去重方法
-	user_array_now:= myjwt.RemoveRepByMapInt(user_array_old)
+	user_array_now := myjwt.RemoveRepByMapInt(user_array_old)
 	beego.Info(user_array_now)
+	beego.Info(len(reply))
 	return nil
 }
